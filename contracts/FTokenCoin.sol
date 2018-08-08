19983b6fb747545675fce6a71b354baf9a538496
Derek Nguyen <quanmn.libra@gmail.com>
2018-08-08 21:47:55 +0700

    fix contract

--- contracts/FTokenCoin.sol
+++ contracts/FTokenCoin.sol
@@ -3,25 +3,23 @@ pragma solidity ^0.4.24;
 contract FTokenCoin {
     mapping (address => uint256) public balanceOf;
 
-    string public name = "First Token Coin";
-    string public symbol = "FTC";
+    string public name = "First Token Coin 2";
+    string public symbol = "FTC2";
     uint8 public decimals = 18;
-    uint256 public unitsOneEthCanBuy;
+    uint256 public unitsOneEthCanBuy = 1000;
     uint256 public totalEthInWei;
     address public fundsWallet;
-
     uint256 public totalSupply = 1000000 * (uint256(10) ** decimals);
 
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor() public {
-        // Initially assign all tokens to the contract's creator.
         balanceOf[msg.sender] = totalSupply;
-        unitsOneEthCanBuy = 100;
+        fundsWallet = msg.sender;
         emit Transfer(address(0), msg.sender, totalSupply);
     }
 
-    function() payable{
+    function() public payable{
         totalEthInWei = totalEthInWei + msg.value;
         uint256 amount = msg.value * unitsOneEthCanBuy;
         require(balanceOf[fundsWallet] >= amount);
@@ -47,14 +45,16 @@ contract FTokenCoin {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
     mapping(address => mapping(address => uint256)) public allowance;
+    mapping (address => mapping (address => uint256)) allowed;
 
-    function approve(address spender, uint256 value)
-        public
-        returns (bool success)
-    {
-        allowance[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
         return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
     }
 
     function transferFrom(address from, address to, uint256 value)
@@ -68,6 +68,13 @@ contract FTokenCoin {
         balanceOf[to] += value;
         allowance[from][msg.sender] -= value;
         emit Transfer(from, to, value);
+        return true;
+    }
+
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
         return true;
     }
 }
